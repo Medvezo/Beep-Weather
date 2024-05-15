232c78c6c05ee70d3e5f5f16fd7a2bed547a5944
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-05-15 23:26:08 +0400

    feat: manage weather state in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-function App() {
+import { useState } from "react";
+import SearchForm from "./components/SearchForm";
+import WeatherCard from "./components/WeatherCard";
 
-  return (
-    <>
-     
-    </>
-  )
+function App() {
+	const [weather, setWeather] = useState("");
+
+	return (
+		<main className="flex flex-col">
+			<SearchForm setWeather={setWeather} />
+			<WeatherCard weather={weather} />
+		</main>
+	);
 }
 
-export default App
+export default App;
