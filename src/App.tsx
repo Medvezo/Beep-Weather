e4956385f1b14013ca31b50438ad3d7abfdecc1a
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-05-16 00:38:30 +0400

    style: advanced styling for WeatherCard

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 	const [weather, setWeather] = useState("");
 
 	return (
-		<main className="flex flex-col">
+		<main className="flex flex-col gap-52 mt-20 min-h-screen">
 			<SearchForm setWeather={setWeather} />
 			<WeatherCard weather={weather} />
 		</main>
--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -26,11 +26,11 @@ export default function WeatherCard(weather: any) {
 		Tornado: "🌪️",
 	};
 
-	const getWeatherIcon = (main:any) => weatherIcons[main] || "🌍";
+	const getWeatherIcon = (main: any) => weatherIcons[main] || "🌍";
 
 	return (
-		<section className="flex flex-col gap-10 bg-white p-5 rounded-lg text-black min-w-96">
-			<div className="flex gap-10 divide-x-2 divide-slate-950/80 p-5">
+		<section className="flex flex-col gap-10 bg-white p-5 rounded-lg text-black w-full px-3 min-w-80 sm:min-w-96">
+			<div className="flex sm:flex-row flex-col gap-10 sm:divide-x-2 divide-slate-950/80 p-5">
 				<div className="flex  justify-center items-center gap-5">
 					<h2 className="text-3xl">{data.name}</h2>
 					<p className="text-3xl ">
@@ -49,23 +49,23 @@ export default function WeatherCard(weather: any) {
 				</aside>
 			</div>
 
-			<aside className="flex justify-between items-center p-5 rounded-xl bg-slate-300 ">
-				<div className="flex flex-col justify-center items-center">
-					<p className="text-xl ">Feels like:</p>
+			<aside className="flex justify-between items-center p-5 rounded-xl bg-slate-300 px-3 sm:flex-row flex-col gap-5 sm:gap-0 divide-y-2 sm:divide-x-2 sm:divide-y-0 divide-slate-600/30 ">
+				<div className="flex flex-col justify-center items-center p-1">
+					<p className="text-xl ">🌡️ Feels like:</p>
 					<p className="text-lg font-bold">
 						{isValidMain && data.main.feels_like.toFixed()}°C
 					</p>
 				</div>
 
-				<div className="flex flex-col justify-center items-center">
-					<p className="text-xl ">Humidity:</p>
+				<div className="flex flex-col justify-center items-center px-3">
+					<p className="text-xl ">💧 Humidity:</p>
 					<p className="text-lg font-bold">
 						{isValidMain && data.main.humidity}%
 					</p>
 				</div>
 
-				<div className="flex flex-col justify-center items-center">
-					<p className="text-xl ">Wind:</p>
+				<div className="flex flex-col justify-center items-center px-3">
+					<p className="text-xl ">💨 Wind:</p>
 					<p className="text-lg font-bold">
 						{data.wind && data.wind.speed.toFixed()} KM/H{" "}
 					</p>
