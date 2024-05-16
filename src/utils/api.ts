9b63c21293ee590f9ffd1be7e7340ebefe5ff60e
Medvezo <mansur.mustafayev@gmail.com>
2024-05-16 10:48:48 +0400

    feat: cache n retrieve data in local storage for 1 hour

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,18 @@
 import axios from "axios";
 
 export async function getWeather(city: string) {
+	// checking if there is already weather data in localStorage
+	const cachedData = localStorage.getItem(city);
+	if (cachedData) {
+		const cachedWeather = JSON.parse(cachedData);
+		const currentTime = new Date().getTime();
+		// check if cached data is less than 60 minutes old
+		if (currentTime - cachedWeather.timestamp < 3600000) {
+			console.log("Returned cache data")
+			return cachedWeather.data;
+		}
+	}
+
 	try {
 		const response = await axios.get(
 			`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
@@ -11,10 +23,18 @@ export async function getWeather(city: string) {
 		const data = response.data;
 		if (!data) throw new Error("Response has empty body");
 
-		return data;
+		// save to local storage with a time
+		localStorage.setItem(
+			city,
+			JSON.stringify({
+				data: data,
+				timestamp: new Date().getTime(),
+			})
+		);
 
+		return data;
 	} catch (error) {
 		console.error(error);
-		return error
+		return error;
 	}
 }
