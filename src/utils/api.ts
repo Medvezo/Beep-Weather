bc69ec74d853ff4fd159494778b5367369446f4d
Medvezo <mansur.mustafayev@gmail.com>
2024-05-16 18:20:01 +0400

    feat: add basic getSuggestions function to fetch suggestions

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import React from "react";
 
 export async function getWeather(city: string) {
 	getLocalStorage(city);
@@ -14,7 +15,7 @@ export async function getWeather(city: string) {
 		if (!data) throw new Error("Response has empty body");
 
 		setLocalStorage(data, city);
-		
+
 		return data;
 	} catch (error) {
 		console.error(error);
@@ -47,4 +48,30 @@ function setLocalStorage(data: any, city: string) {
 			timestamp: new Date().getTime(),
 		})
 	);
+}
+
+export async function getSuggestions(
+	value: string,
+	setState: React.Dispatch<React.SetStateAction<any>>
+) {
+	if (value.length > 2) {
+		// Only fetch if input length > 2
+		try {
+			const response = await axios.get(
+				`https://test.api.amadeus.com/v1/reference-data/locations/cities?keyword=${value}&max=5`,
+				{
+					headers: {
+						Authorization: `Bearer ${import.meta.env.VITE_AMADEUS_KEY}`,
+					},
+				}
+			);
+			console.log(response.data)
+			setState(response.data.data);
+		} catch (error) {
+			console.error("Error fetching cities:", error);
+			setState([]);
+		}
+	} else {
+		setState([]);
+	}
 }
