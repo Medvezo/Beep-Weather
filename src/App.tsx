fd38a74a7a36cb4b42b96698ed772c10aee1cd65
Medvezo <mansur.mustafayev@gmail.com>
2024-05-16 10:34:57 +0400

    build: add react-toastify to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import SearchForm from "./components/SearchForm";
 import WeatherCard from "./components/WeatherCard";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 	const [weather, setWeather] = useState("");
 
 	return (
-		<main className="flex flex-col gap-52 mt-20 min-h-screen">
+		<main className="flex flex-col justify-start items-center gap-10 sm:gap-16 mt-20 min-h-screen">
 			<SearchForm setWeather={setWeather} />
 			<WeatherCard weather={weather} />
+			<ToastContainer theme="colored"/>
 		</main>
 	);
 }
