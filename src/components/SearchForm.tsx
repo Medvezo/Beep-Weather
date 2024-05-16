03701c0608b9b9ddc1a46828832a82cd137a9bf2
Medvezo <mansur.mustafayev@gmail.com>
2024-05-16 09:32:37 +0400

    style: advanced styling for SearchForm component

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -14,26 +14,34 @@ export default function SearchForm({
 	};
 
 	const handleSubmit = async (e: any) => {
-		e.preventDefault(); // preventing refresh
+		e.preventDefault(); // Preventing refresh
 
 		const weatherData = await getWeather(search);
 		setWeather(weatherData);
 	};
 
 	return (
-		<form className="flex flex-col">
-			<label htmlFor="weather">Enter city name:</label>
+		<form className="flex flex-col p-5 shadow-lg rounded-lg bg-gradient-to-b from-white  to-teal-500   max-w-md mx-auto mt-10">
+			<label
+				htmlFor="weather"
+				className="text-lg text-slate-950 font-semibold mb-2">
+				Enter a city name:
+			</label>
 			<input
 				type="text"
 				id="weather"
 				name="weather"
 				value={search}
 				onChange={handleSearch}
+				className="p-2 border border-slate-300 rounded focus:outline-none focus:border-teal-500 focus:ring-1 focus:ring-teal-500 transition-all duration-200 ease-in-out mb-4"
+				placeholder="Prague"
 			/>
-			<button type="submit" onClick={handleSubmit}>
+			<button
+				type="submit"
+				onClick={handleSubmit}
+				className="bg-teal-700 text-white rounded p-2 hover:bg-teal-800 focus:outline-none focus:ring-2 focus:ring-teal-900 focus:ring-opacity-50 transition-all duration-150 ease-in-out">
 				Search
 			</button>
-
 		</form>
 	);
 }
